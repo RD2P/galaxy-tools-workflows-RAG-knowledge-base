57c41f430331e4f5921c6eb9733b25095611428b
Raphael <gld141@mail.usask.ca>
2026-06-18 17:03:20 -0600

    create faiss index and save to file

--- embed.py
+++ embed.py
@@ -14,7 +14,10 @@ model = SentenceTransformer(
 
 # create embeddings
 tools: list[str] = parse_tools("data/tools.txt")
-
 texts: list[str] = [to_embedding_text(tool) for tool in tools]
-
 embeddings = model.encode(texts, convert_to_numpy=True)
+
+# create index and save to file
+index = faiss.IndexFlatL2(embeddings.shape[1])
+index.add(embeddings)
+faiss.write_index(index, "data/tools.faiss")
