1868392b1e6de51d3d3fa0e2c774c9911281800f
Raphael <gld141@mail.usask.ca>
2026-06-18 17:58:01 -0600

    take query intput from cli

--- main.py
+++ main.py
@@ -10,24 +10,29 @@ model = sentence_transformers.SentenceTransformer(
     "sentence-transformers/all-MiniLM-L6-v2",
     token=os.getenv("HF_TOKEN"))
 
-query = "Which tools are used for quality control?"
-query_embedding = model.encode(
-    [query], 
-    convert_to_numpy=True, 
-    show_progress_bar=True)
- 
+# Load the FAISS index and tools
 index = faiss.read_index("data/tools.faiss")
-distances, indices = index.search(query_embedding, k=5)
- 
-print("Query:", query)
-print("distances:\n", distances)
-print("indices:\n", indices)
- 
 tools = parse_tools("data/tools.txt")
 texts = [to_embedding_text(tool) for tool in tools]
 
-results = [texts[i] for i in indices[0]]
+while True:
+    query = input("\n\nEnter your query (or 'exit' to quit): ")
+    if query.lower() == 'exit':
+        break
 
-for result in results:
-    print(result)
-    print("=" * 80)
+    query_embedding = model.encode(
+        [query], 
+        convert_to_numpy=True, 
+        show_progress_bar=True)
+    
+    distances, indices = index.search(query_embedding, k=5)
+    
+    print("Query:", query)
+    # print("Distances:\n", distances)
+    # print("Indices:\n", indices)
+    
+    results = [texts[i] for i in indices[0]]
+
+    for result in results:
+        print(result)
+        print("=" * 80)
